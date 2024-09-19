36252b919bc15d1bd42b7c4f126b98182b4bfd89
keelann95 <sephen.mwaniki@student.moringaschool.com>
2024-09-19 21:14:02 +0300

    created this file to set up a database using SQLAlchemy

---
+++ models/__init__.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base = declarative_base()
+
+def initialize_database():
+    from models.contact import Contact
+    from models.group import Group
+    Base.metadata.create_all(engine)
