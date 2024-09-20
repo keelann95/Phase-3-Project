acd50bde7bab8e81b5af504017229f68cd65bc47
keelann95 <sephen.mwaniki@student.moringaschool.com>
2024-09-20 10:04:15 +0300

    defined the database url

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+DATABASE_URL = "sqlite:///contacts.db"
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
 session = Session()
